b3d6c0d31bf0a7d4c69e57ce801a4a9d84617779
Smirithi <smirithiramasubramanian@gmail.com>
2021-01-17 07:32:10 +0530

    Add files - initial commit

---
+++ specs/github.spec
@@ -0,0 +1,10 @@
+# Github Login
+
+* Navigate to github login page "https://github.com/login"
+* Verify page heading to be "Sign in to GitHub"
+
+## SignIn to github account
+
+* Enter credentials "smirithi898@gmail.com" and "Sm1r1th1@098"
+* Click to SignIn
+* Verify landing page after signIn
---
+++ tests/githubLogin.js
@@ -0,0 +1,37 @@
+const pptr = require('puppeteer');
+const assert = require('assert');
+
+let page;
+let browser;
+
+beforeSuite(async function() {
+    browser = await pptr.launch({headless: false, slowMo: 50});
+    page = await browser.newPage();
+});
+
+step("Navigate to github login page <url>", async function(url){
+    await page.goto(url);
+});
+
+step("Verify page heading to be <heading>", async function(heading){
+    await page.waitForSelector('div[class*="form-header"] h1');
+    const pageHeading = await page.$eval('div[class*="form-header"] h1', el => el.innerText);
+    assert.strictEqual(pageHeading, heading);
+});
+
+step("Enter credentials <email> and <password>", async function(email, password) {
+    await page.type('input#login_field', email);
+    await page.type('input#password', password);
+});
+
+step("Click to SignIn", async function(){
+    await page.click('input[name="commit"]');
+});
+
+step("Verify landing page after signIn", async function() {
+    await page.waitForSelector('div#dashboard-repos-container');
+});
+
+afterSuite(async function(){
+    browser.close();
+})
